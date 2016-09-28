8edccfa59a2095c7d48d75dd249c184e57fc047f
jfairburn12 <jfairburn@gmail.com>
2016-09-28 20:10:36 +1300

    weather

--- App/Scripts.js
+++ App/Scripts.js
@@ -11,4 +11,31 @@
             window.location.hash = target;
         });
     });
+
 });
+
+//Change the tile background colour using the jQuery.Color Plugin
+function setTileColour(tile) {
+    var desc = $(tile).find(".desc").html();
+    if (desc.includes("rain")) {
+        jQuery(tile).animate({
+            backgroundColor: jQuery.Color("#9B9AB3")
+        }, 500);
+    } else if (desc.includes("cloud")) {
+        jQuery(tile).animate({
+            backgroundColor: jQuery.Color("#ddd")
+        }, 500);
+    } else if (desc.includes("clear")) {
+        jQuery(tile).animate({
+            backgroundColor: jQuery.Color("#0092D7")
+        }, 500);
+    } else if (desc.includes("snow")) {
+        jQuery(tile).animate({
+            backgroundColor: jQuery.Color("#D4E5FF")
+        }, 500);
+    } else {
+        jQuery(tile).animate({
+            backgroundColor: jQuery.Color("#fff")
+        }, 500);
+    }
+}
